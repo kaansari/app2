62246a884455a53baf0d4486da199989f62f3ffa
Khalid Ansari <kaansari@KhalidMini.local>
2016-10-17 00:00:16 -0400

    First upload

---
+++ test/heartbeat.js
@@ -0,0 +1,23 @@
+var app = require('../app.js'), request = require('supertest');
+
+describe('vision heartbeat api', function(){
+  describe('when requesting resource /heartbeat', function(){
+    it('should respond with 200', function(done){
+      request(app)
+      .get('/heartbeat')
+      .expect('Content-Type', /json/)
+      .expect(200, done);
+    });
+  });
+  describe('when not found requesting resource /missing', function() {
+    it('should respond with 404', function (done) {
+      request(app)
+          .get('/missing')      
+          .expect(404,done);
+    });
+  });
+
+});
+
+
+
